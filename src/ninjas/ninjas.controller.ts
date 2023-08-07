389f5ad702c8ff18fe0b1e0c8e6375def5545537
RyanHaniff <haniffryan.hr@gmail.com>
2023-08-07 10:52:45 -0400

    basic controller setup

--- src/ninjas/ninjas.controller.ts
+++ src/ninjas/ninjas.controller.ts
@@ -1,4 +1,47 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, Query } from '@nestjs/common';
+import { CreateNinjaDto } from './dto/create-ninja.dto';
+import { UpdateNinjaDto } from './dto/update-ninja.dto';
 
 @Controller('ninjas')
-export class NinjasController {}
+export class NinjasController {
+    // controllers are in charge of defining the paths 
+
+    // GET /ninjas?type=fast --> []
+     @Get() // we have to tell nestJS that is is a get request
+    getNinjas(@Query('type') type: string) {
+        return [{type}];
+    }
+    
+    // GET /ninjas/:id --> {...}
+    @Get(':id') // having :id defines the 
+    getOneNinja(@Param('id') id: string) {
+        return {
+            id
+        };
+    }
+
+    // POST (create ninjas) /ninjas
+    @Post()
+    createNinja(@Body() createNinjaDto: CreateNinjaDto){
+        return {
+            name: createNinjaDto.name
+        };
+    }
+
+    // PUT /ninjas/:id --> {...}
+    @Put(':id')
+    updateNinja(@Param(':id') id: string, @Body() updateNinjaDto: UpdateNinjaDto){
+        return {
+            id,
+            name: updateNinjaDto
+        };
+    }
+
+    // DELETE /ninjas/:id
+    @Delete()
+    removeNinja(@Param(':id') id: string) {
+        return{
+            id
+        };
+    }
+}
